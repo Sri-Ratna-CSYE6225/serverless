7af9e32878d78584741995c1c9066b800c9bca31
ASR98 <43495517+ASR98@users.noreply.github.com>
2022-04-20 19:31:46 -0400

    changed http from https

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ exports.handler = (event, context, callback) => {
 
 var sendEmail = (data) => {
 
-    let link = `http://${data.domainName}/v1/verifyUserEmail?email=${data.username}&token=${data.token}`;
+    let link = `https://${data.domainName}/v1/verifyUserEmail?email=${data.username}&token=${data.token}`;
 
     let body = "Hi " + data.first_name + ",\n\n" +
     "We just need to verify your email address before you can access our portal. Verify your email address here: " + link + "\n\n\n" +
